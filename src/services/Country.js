b29c9cf6f47d9b8ad78523a43491f36e224d601d
Betoml5 <betoml5@hotmail.com>
2022-06-15 15:58:08 -0500

    Add fundamental folders

---
+++ src/services/Country.js
@@ -0,0 +1,31 @@
+const API_BASE = "https://restcountries.com/v3.1";
+
+export const getAllCountriesAPI = async () => {
+  try {
+    const response = await fetch(`${API_BASE}/all`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getCoutryByName = async (name) => {
+  try {
+    const response = await fetch(`${API_BASE}/name/${name}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getCoutryByRegion = async (region) => {
+  try {
+    const response = await fetch(`${API_BASE}/region/${region}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
